250c95111c7ebd48609afd9170cd20e53606e26a
Ere Maijala <ere.maijala@helsinki.fi>
2020-06-02 15:12:07 +0300

    Fix curl tests.

--- src/BeSimple/SoapClient/Tests/AbstractWebserverTest.php
+++ src/BeSimple/SoapClient/Tests/AbstractWebserverTest.php
@@ -47,7 +47,7 @@ abstract class AbstractWebServerTest extends \PHPUnit\Framework\TestCase
         ))->getProcess();
 
         self::$webserver->start();
-        usleep(100000);
+        sleep(1);
 
         self::$websererPortLength = strlen(WEBSERVER_PORT);
     }
@@ -55,6 +55,6 @@ abstract class AbstractWebServerTest extends \PHPUnit\Framework\TestCase
     public static function doTearDownAfterClass()
     {
         self::$webserver->stop(0);
-        usleep(100000);
+        sleep(1);
     }
 }
